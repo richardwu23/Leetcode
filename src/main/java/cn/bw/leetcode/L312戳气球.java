e4abd792d83ea3a7088e159f45b4de9759d2f5ee
richi <120793495@qq.com>
2023-08-12 12:38:25 +0800

    leetcode practice  first commit

---
+++ src/main/java/cn/bw/leetcode/L312戳气球.java
@@ -0,0 +1,33 @@
+package cn.bw.leetcode;
+
+public class L312戳气球 {
+
+    // leetcode 312
+    //https://leetcode.com/problems/burst-balloons/description/?source=submission-noac
+    // solve: 1. dp;  dp[i][j] means  (i,j) max.
+
+    public int maxCoins(int[] nums) {
+
+        int len=nums.length;
+        int[] s = new int[len+2];
+        s[0]=s[len+1]=1;
+        for(int i=1;i<=len;i++){
+            s[i]=nums[i-1];
+        }
+        int[][] dp = new int[len+2][len+2];
+
+
+        for(int i=1;i<=len;i++){
+            for(int left=1;left+i-1<=len;left++){
+                int right=left+i-1;
+                for(int k=left;k<=right;k++){
+                    dp[left][right]= Math.max(dp[left][right],
+                            dp[left][k-1] + dp[k+1][right]+ s[left-1]*s[right+1]*s[k]);
+                }
+            }
+        }
+
+        return dp[1][len];
+    }
+
+}
